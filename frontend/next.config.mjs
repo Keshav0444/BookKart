7a28664bec2d80cde9cc568f314d4ad15c380433
desistack7 <desistack7@gmail..com>
2025-03-26 16:57:55 +0530

    fixed next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode:true,
+  eslint:{
+    ignoreDuringBuilds:true,
+  },
   images: {
     domains: ['lh3.googleusercontent.com','res.cloudinary.com','images.unsplash.com','media.istockphoto.com'], 
   },
