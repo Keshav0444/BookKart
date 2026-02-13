1407da20daa19ae9612e99b6edf3322d1de72f1e
Keshav0444 <kk8802524@gmail.com>
2026-02-13 17:14:16 +0530

    Change in google strategy

--- frontend/src/store/adminApi.ts
+++ frontend/src/store/adminApi.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api' || 'https://bookkart-ug7b.onrender.com/api';
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
 
 export const adminApi = createApi({
     reducerPath:'adminApi',
